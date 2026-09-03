378385a8a7c3b3fee4e15aa52f55fd65108eb0c8
piloujp <117885587+piloujp@users.noreply.github.com>
2026-09-03 17:29:58 +0900

    French and Japanese translations
    The constant added in PR #20 is already included.
    Perhaps, before merging new PRs, Folder name and manifest file should be updated to 1.0.1.

---
+++ zc_plugins/ZxSeoMaster/v1.0.0/admin/includes/languages/japanese/extra_definitions/lang.menu.php
@@ -0,0 +1,23 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_ZXSEOMASTER' => 'ZX SEOマスター',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_ZXSEOMASTER' => 'Zen Cart向けの、一元化され完全にカプセル化されたオンサイトSEOスイート。',
+//  BOF ZX Seo Master- configuration
+    'CFGTITLE_ZX_SEO_MASTER_CUSTOM_META_TAGS' => 'ZX SEO カスタムメタタグ',
+    'CFGDESC_ZX_SEO_MASTER_CUSTOM_META_TAGS' => 'HTMLヘッダーに挿入されるグローバルなカスタムメタタグ。',
+    'CFGTITLE_ZX_SEO_MASTER_CUSTOM_FOOTER_SCRIPTS' => 'ZX SEO カスタムフッタースクリプト',
+    'CFGDESC_ZX_SEO_MASTER_CUSTOM_FOOTER_SCRIPTS' => '終了 `body` タグの直前に挿入される、グローバルなカスタム JavaScript。',
+    'CFGTITLE_ZX_INDEXNOW_STATUS' => 'IndexNowの自動送信を有効にする',
+    'CFGDESC_ZX_INDEXNOW_STATUS' => '商品やカテゴリの更新情報をIndexNowに自動送信します。',
+    'CFGTITLE_ZX_INDEXNOW_ENDPOINT' => 'IndexNow エンドポイント',
+    'CFGDESC_ZX_INDEXNOW_ENDPOINT' => 'IndexNowエンドポイントを選択してください（いずれか一つに送信すれば、すべてに共有されます）。',
+    'CFGTITLE_ZX_INDEXNOW_KEY' => 'IndexNowキー',
+    'CFGDESC_ZX_INDEXNOW_KEY' => '自動生成されたIndexNowキー。対応するtxtファイルがストアのルートディレクトリに存在する必要があります。',
+    'CFGTITLE_ZX_SEO_MASTER_GLOBAL_NOINDEX' => 'グローバル Noindex',
+    'CFGDESC_ZX_SEO_MASTER_GLOBAL_NOINDEX' => 'グローバルNoindexスイッチh',
+    'CFGTITLE_ZX_SEO_MASTER_GLOBAL_NOFOLLOW' => 'グローバルNofollow',
+    'CFGDESC_ZX_SEO_MASTER_GLOBAL_NOFOLLOW' => 'グローバルNofollowスイッチ',
+//  EOF ZX Seo Master- configuration
+];
+
+return $define;
